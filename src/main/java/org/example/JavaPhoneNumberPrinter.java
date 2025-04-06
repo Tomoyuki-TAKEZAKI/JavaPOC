6f7c4c2bad3c6087754fd279c35ea41aa944157f
t-takezaki <49390028+Tomoyuki-TAKEZAKI@users.noreply.github.com>
2025-04-06 14:39:52 +0900

    Add a factory method with @JvmStatic annotation

--- src/main/java/org/example/JavaPhoneNumberPrinter.java
+++ src/main/java/org/example/JavaPhoneNumberPrinter.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.example.phone.PhoneNumber;
+
 public class JavaPhoneNumberPrinter {
     void run() {
-        // This code fails to compile!
-//        PhoneNumber phoneNumber = new PhoneNumber("123-456-7890");
-//        System.out.println("Phone Number: " + phoneNumber.getValue());
+        // This code still fails to compile.
+        String phoneNumber = PhoneNumber.Companion.of("123-456-7890");
+        System.out.println("Phone Number: " + phoneNumber);
     }
 }
--- src/main/java/org/example/phone/PhoneNumber.kt
+++ src/main/java/org/example/phone/PhoneNumber.kt
@@ -3,4 +3,10 @@ package org.example.phone
 @JvmInline
 value class PhoneNumber(
     val value: String
-)
+) {
+    // Add a factory method with @JvmStatic annotation
+    companion object {
+        @JvmStatic
+        fun of(value: String): PhoneNumber = PhoneNumber(value)
+    }
+}
